52b1e34583e172348b8afc7c83d495ca95d48b16
ChimataSrivalli <153272032+ChimataSrivalli@users.noreply.github.com>
2026-06-29 11:44:05 +0530

    Update app.py

--- k8s-app/app.py
+++ k8s-app/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Terraform + Jenkins + Minikube Project"
+    return "Welcome to CICD Terraform Project"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+@app.route('/health')
+def health():
+    return "Application is Healthy"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
